ce15034a3a29b2005dfae4b8872fbf1e4dc242d9
ManasiBalajiUpare <162618411+ManasiBalajiUpare@users.noreply.github.com>
2025-08-28 17:13:33 +0530

    add about and contact page
    add pages

--- src/controller/homepagectrl.js
+++ src/controller/homepagectrl.js
@@ -2,3 +2,11 @@ exports.homepage=(req,res)=>{
     res.render("homepage");
     
 }
+exports.aboutPage=(req,res)=>{
+    res.render("about");
+
+}
+exports.contactPage=(req,res)=>{
+    res.render("contact");
+
+}
